747c08e3d96a2416dffeb35995f981930975954b
Remy Guts <remyguts@gmail.com>
2019-12-17 12:54:42 -0800

    updated start button and attatched js code to it

---
+++ questions.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  $('#start-button').on('click', function() {
+//how much of this do I keep inside this set of brackets?
+
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    }
+];
+
+var score = 0;
+
+
+});
