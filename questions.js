db64cbdfaaef0ab3917338d4cfb35eeb1f8a9773
Remy Guts <remyguts@gmail.com>
2019-12-19 17:17:46 -0800

    fixed timer got rid of answer buttons

--- questions.js
+++ questions.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
-  
   //var timer = Object(timer);
-  var startBtn = document.getElementById('startBtn');
-  var answerBtnOne = document.getElementById('answer-buttonone');
-  var answerBtnTwo = document.getElementById('answer-buttontwo');
-  var answerBtntThree = document.getElementById('answer-buttonthree');
-  var answerBtnFour = document.getElementById('answer-buttonfour');
-  var highscoreBtn = document.getElementById('highscoreBtn');
-  var c = 200;
-  startBtn.addEventListener("click", startQuiz);
-//start game is here
+  var startBtn = document.getElementById("startBtn");
+  var answerBtnOne = document.getElementById("answer-buttonone");
+  var answerBtnTwo = document.getElementById("answer-buttontwo");
+  var answerBtntThree = document.getElementById("answer-buttonthree");
+  var answerBtnFour = document.getElementById("answer-buttonfour");
+  var highscoreBtn = document.getElementById("highscoreBtn");
+  var timerElement = document.getElementById("timer");
+  var currentTime = 200;
+  var score = 0;
+
+  //start game is here
   function startQuiz() {
     //console.log('Started')
-    
-    startBtn.classList.add('hide');
 
-  
+    startBtn.classList.add("hide");
+    setInterval(timer, 1000);
 
-    
+    // generateQueztion()
+
     document.getElementById("startBtn").style.visibility = "hidden";
     document.getElementById("answer-buttonone").style.visibility = "visible";
     document.getElementById("answer-buttontwo").style.visibility = "visible";
@@ -25,7 +26,7 @@ $(document).ready(function() {
     document.getElementById("answer-buttonfour").style.visibility = "visible";
   }
   //question area
-var questions = [
+  var questions = [
     {
       title: "Commonly used data types DO NOT include:",
       choices: ["strings", "booleans", "alerts", "numbers"],
@@ -36,19 +37,20 @@ var questions = [
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
     }
-];
-//timer area
-function timer001() {
-  c = c - 1;
-  if (c < 200) {
-    timer.innerHTML = c;
-  }
-  if (c < 1) {
-    window.clearInterval(update);
-  }
-}
-update = setInterval("timer001()", 1000);
-var score = 0;
-})
+  ];
+  //timer area
+  function timer() {
+    // Decrement time
+    currentTime--;
+    // Update timer element with current time
+    timerElement.textContent = currentTime;
 
-;
+    if (currentTime === 0) {
+      // Run function Game Over
+    }
+  }
+
+
+  startBtn.addEventListener("click", startQuiz);
+
+});
