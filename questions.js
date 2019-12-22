c700b2a9131e425be69ba012311308d319d51835
Remy Guts <remyguts@gmail.com>
2019-12-21 23:46:48 -0800

    last minute debugging

--- questions.js
+++ questions.js
@@ -1,25 +1,23 @@
-$(document).ready(function() {
-  
+$(document).ready(function () {
   var startBtn = document.getElementById("startBtn");
   var highscoreBtn = document.getElementById("highscoreBtn");
   var timerElement = document.getElementById("timer");
   var currentTime = 200;
   var questionElement = document.getElementById("questions");
-  var answerButtonsEl = document.getElementById("answer-buttons")
+  var answerButtonsEl = document.getElementById("answer-buttons");
   var score = 0;
-  var initials = "" ;
-  var highScore = document.getElementById('highscore')
+  var initials = "";
+  var highScore = document.getElementById("highscore");
   var questionIndex = 0;
-  var timerId  ;
+  var timerId;
   startBtn.addEventListener("click", startQuiz);
   //start game is here
   function startQuiz() {
     //console.log('Started')
 
     startBtn.classList.add("hide");
-    
-    timerId = setInterval(timer, 1000);
 
+    timerId = setInterval(timer, 1000);
 
     // generateQueztion()
     generateQuestions();
@@ -28,7 +26,6 @@ $(document).ready(function() {
   function generateQuestions() {
     questionElement.textContent = "";
 
-    
     // Empty it
     // Replace it with new title from questions object
 
@@ -41,40 +38,38 @@ $(document).ready(function() {
     questionElement.textContent = questions[0].title;
     let currentQuestionChoices = questions[0].choices;
     for (i in currentQuestionChoices) {
-      var choice = document.createElement('button');
+      var choice = document.createElement("button");
       choice.textContent = currentQuestionChoices[i];
-      choice.addEventListener('click', function(event) {
+      choice.addEventListener("click", function (event) {
+        if (questions[0].answer === event.currentTarget.innerText) {
+          console.log("right answer");
+        } else {
+          console.log("wrong answer");
 
-        
-        if (questions[0].answer === event.currentTarget.innerText) 
-      {
-        console.log("right answer");
-         }
-      else {
-        console.log("wrong answer");
+          //minus off current time variable
 
-       
-        //minus off current time variable
+          currentTime = currentTime - 15;
+        }
+        questionIndex++;
+        console.log("what question?", questionIndex);
+        questionElement.textContent = questions[questionIndex].title;
+        answerButtonsEl;
 
-currentTime= currentTime - 15
-      }
-      questionIndex++;
-        console.log("what question?",questionIndex);
-      questionElement.textContent = questions[questionIndex].title;
-      answerButtonsEl;
-
-      console.log(answerButtonsEl.children);
-      answerButtonsEl.children[0];
-      console.log(answerButtonsEl.children[0]);
-      answerButtonsEl.children[0].textContent = questions[questionIndex].choices[0];
-      answerButtonsEl.children[1].textContent = questions[questionIndex].choices[1]; 
-      answerButtonsEl.children[2].textContent = questions[questionIndex].choices[2];
-      answerButtonsEl.children[3].textContent = questions[questionIndex].choices[3];
-    })
+        console.log(answerButtonsEl.children);
+        answerButtonsEl.children[0];
+        console.log(answerButtonsEl.children[0]);
+        answerButtonsEl.children[0].textContent =
+          questions[questionIndex].choices[0];
+        answerButtonsEl.children[1].textContent =
+          questions[questionIndex].choices[1];
+        answerButtonsEl.children[2].textContent =
+          questions[questionIndex].choices[2];
+        answerButtonsEl.children[3].textContent =
+          questions[questionIndex].choices[3];
+      });
 
       answerButtonsEl.appendChild(choice);
     }
-
 
     // for (var i = 0; i < questions.choices.length; ++i) {
     //   var answerBtn = $("<button>");
@@ -82,8 +77,8 @@ currentTime= currentTime - 15
     //   $("#buttons").append(buttonElement);
     // }
   }
-// if answer clicked is the same as the "answer" for that current question, then move to next question and add to score. 
-// once done with question advance to next number in index
+  // if answer clicked is the same as the "answer" for that current question, then move to next question and add to score.
+  // once done with question advance to next number in index
 
   var questions = [
     {
@@ -110,11 +105,8 @@ currentTime= currentTime - 15
       title: "which is not a source for coding information?",
       choices: ["W3 Schools", "Code Academy", "Stack Overflow", "Spotify"],
       answer: "alerts"
-    },
+    }
   ];
-
-
-
 
   //timer area
   function timer() {
@@ -123,75 +115,60 @@ currentTime= currentTime - 15
     // Update timer element with current time
     timerElement.textContent = currentTime;
 
-    if (currentTime <= 0 || questionIndex === questions.length )  {
-      
-        alert('Game Over');
-        localStorage.setItem("highscore",currentTime);
-        clearInterval(timerId);
-        initials += prompt('what is your intitals?');
-        
-    //console.log(initials);
+    if (currentTime <= 0 || questionIndex === questions.length) {
+      alert("Game Over");
+      localStorage.setItem("highscore", currentTime);
+      clearInterval(timerId);
+      initials += prompt("what is your intitals?");
 
-    localStorage.setItem(initials);
-   
+      //console.log(initials);
+
+      localStorage.setItem(initials);
+
       // Run function Game Over
     }
   }
 
-  
-
   // ... stores initials local storage
-// displays initials and time on highscore sheet
+  // displays initials and time on highscore sheet
 
-console.log(timerElement);
+  console.log(timerElement);
 
+  const form = document.querySelector("form");
+  const ul = document.querySelector("ul");
+  const button = document.querySelector("button");
+  const input = document.getElementById("item");
 
-const form = document.querySelector('form')
-const ul = document.querySelector('ul')
-const button = document.querySelector('button')
-const input = document.getElementById('item')
+  const liMaker = text => {
+    const li = document.createElement("li");
+    li.textContent = text;
+    ul.appendChild(li);
+  };
 
-const liMaker = text => {
-  const li = document.createElement('li')
-  li.textContent = text
-  ul.appendChild(li)
-};
+  //form.addEventListener("submit", function () {
+    // e.preventDefault()
 
-form.addEventListener('submit', function(e) {
-  e.preventDefault()
+    liMaker(input.value);
+    input.value = "";
+  });
+  let itemsArray = [];
 
-  liMaker(input.value)
-  input.value = ''
-});
-let itemsArray = []
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  const data = JSON.parse(localStorage.getItem("items"));
 
-localStorage.setItem('items', JSON.stringify(itemsArray))
-const data = JSON.parse(localStorage.getItem('items'))
+  //e.preventDefault();
 
-e.preventDefault()
+  itemsArray.push(input.value);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
 
-itemsArray.push(input.value)
-localStorage.setItem('items', JSON.stringify(itemsArray))
+  data.forEach(item => {
+    liMaker(item);
+  });
 
-data.forEach(item => {
-  liMaker(item)
-})
-
-button.addEventListener('click', function() {
-  localStorage.clear()
-  while (ul.firstChild) {
-    ul.removeChild(ul.firstChild)
-  }
-})
-
-
-
-
-
-
-
-});
-
-
-
+  button.addEventListener("click", function () {
+    localStorage.clear();
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+  });
 
